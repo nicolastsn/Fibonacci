1b122ccb733918510b146636aa1324922fe7c750
Nicolas <nicolas.tsan@gmail.com>
2017-10-23 19:47:24 +0200

    fields cant be blank, presence true to submit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   include Clearance::User
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :st_number, presence: true
+  validates :st_name, presence: true
+  validates :zip, presence: true
+  validates :city, presence: true
+
 end
